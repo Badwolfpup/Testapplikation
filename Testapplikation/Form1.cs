67e5e903c0902781c548be50e6be1236397aafa8
Badwolfpup <Adam.Folke@hudiksvall.se>
2023-10-06 10:28:02 +0200

    new commit

--- Testapplikation/Form1.cs
+++ Testapplikation/Form1.cs
@@ -3,21 +3,18 @@ namespace Testapplikation
     using System.Drawing;
     public partial class Form1 : Form
     {
-        private bool drawing = false;
-        private Point lastPoint = Point.Empty;
-        private Pen pen = new Pen(Color.Black, 2);
+
         
 
         public Form1()
         {
             InitializeComponent();
-
         }
 
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
+            label1.Text = "Test";
         }
 
 
